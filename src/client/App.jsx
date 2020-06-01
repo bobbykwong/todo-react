efe0a67ecee50c9dedaebb0108532abc54a8b2d3
BOBBY <BOBBYKWONGZW@GMAIL.COM>
2020-06-01 18:55:13 +0800

    able to add input into task array

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -2,10 +2,35 @@ import React from 'react';
 import { hot } from 'react-hot-loader';
 
 class App extends React.Component {
+    constructor(){
+        super()
+
+        this.state = ({
+            taskInput: "",
+            tasks: []
+        })
+
+        this.changeHappened = (event) => {
+            this.setState({
+                taskInput: event.target.value
+            })
+        }
+
+        this.clickHappened = () => {
+            this.setState({
+                tasks: [...this.state.tasks, this.state.taskInput]
+            })
+        }
+    }
+
+
   render() {
+    console.log(this.state)
     return (
       <div>
-        Welcome.
+        <div>{this.state.tasks}</div>
+        <input onChange={(event) => {this.changeHappened(event)}}/>
+        <button onClick={() => {this.clickHappened()}} >Submit</button>
       </div>
     );
   }
